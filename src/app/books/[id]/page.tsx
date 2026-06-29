4237cc1eebbb7aab64fa688e4dca00e9360acfe2
Miguel Martinez <miguel.martinez11@gmail.com>
2026-06-29 10:06:15 +0200

    last modification

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -58,6 +58,7 @@ export default async function BookPage({
   // ---------------------------
   // SHELF ACTION
   // ---------------------------
+ 
   async function changeShelf(
   userId: string,
   bookId: string,
